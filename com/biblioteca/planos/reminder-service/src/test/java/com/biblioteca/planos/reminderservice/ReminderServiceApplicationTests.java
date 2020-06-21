90e11e114c3f4411388ed34358ef468d89e38b28
Pykalisto, Anttoni (k1631083) <anttoni.pykalisto@kcl.ac.uk>
2020-06-21 12:46:19 +0100

    Initial commit to start reminder microservice

---
+++ com/biblioteca/planos/reminder-service/src/test/java/com/biblioteca/planos/reminderservice/ReminderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioteca.planos.reminderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReminderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
