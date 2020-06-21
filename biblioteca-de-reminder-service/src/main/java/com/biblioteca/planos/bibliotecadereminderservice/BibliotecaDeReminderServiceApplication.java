7cb94d50dc5f74891bc1a8532f8696696504242c
Pykalisto, Anttoni (k1631083) <anttoni.pykalisto@kcl.ac.uk>
2020-06-21 13:00:58 +0100

    initial commit

---
+++ biblioteca-de-reminder-service/src/main/java/com/biblioteca/planos/bibliotecadereminderservice/BibliotecaDeReminderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.planos.bibliotecadereminderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaDeReminderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaDeReminderServiceApplication.class, args);
+	}
+
+}
---
+++ biblioteca-de-reminder-service/src/test/java/com/biblioteca/planos/bibliotecadereminderservice/BibliotecaDeReminderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioteca.planos.bibliotecadereminderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaDeReminderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
