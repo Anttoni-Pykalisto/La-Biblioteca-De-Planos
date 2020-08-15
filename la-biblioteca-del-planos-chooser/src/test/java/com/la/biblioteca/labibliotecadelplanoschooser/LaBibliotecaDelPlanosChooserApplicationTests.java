49f4779dbad8b7231972e896824c6b5ad3847ad5
Emma Donovan <emmadonovan@Emmas-MacBook-Pro.local>
2020-08-15 12:16:04 +0100

    el-planos-chooser-numero-dos : initial commit

---
+++ la-biblioteca-del-planos-chooser/src/test/java/com/la/biblioteca/labibliotecadelplanoschooser/LaBibliotecaDelPlanosChooserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.la.biblioteca.labibliotecadelplanoschooser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaBibliotecaDelPlanosChooserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
